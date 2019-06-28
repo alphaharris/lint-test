958f2296df4f3eeb40b6cb8586d98dce6ed2c5e7
Aaron Harris <alphaharris@outlook.com>
2019-06-28 15:59:05 -0500

    added bad code

--- code_test.py
+++ code_test.py
@@ -7,3 +7,9 @@ y = list(np.cumsum(x))
 
 #OUTPUT
 print(y)
+
+x=2
+#reallyshoulcatch this
+camelCaseVariableIsWrong = 0
+# this line is long
+#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
